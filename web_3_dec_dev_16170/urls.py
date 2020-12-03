837a0c7a2b4a623f9306fd2f0827c787d680273a
Crowdbotics <team@crowdbotics.com>
2020-12-03 07:15:51 +0000

    Update admin title

--- web_3_dec_dev_16170/urls.py
+++ web_3_dec_dev_16170/urls.py
@@ -32,9 +32,9 @@ urlpatterns = [
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
 ]
 
-admin.site.site_header = "web 5 dec"
-admin.site.site_title = "web 5 dec Admin Portal"
-admin.site.index_title = "web 5 dec Admin"
+admin.site.site_header = "web 3 dec"
+admin.site.site_title = "web 3 dec Admin Portal"
+admin.site.index_title = "web 3 dec Admin"
 
 # swagger
 api_info = openapi.Info(
